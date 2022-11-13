2a67d05155bfdc9c93888c0b4a125ee792bb252d
allan8araujo <allanzao08@gmail.com>
2022-11-13 19:50:29 -0300

    add listTile

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: const Text('teste'),
+      body: Column(
+        children: const [
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.check_box_outline_blank_rounded),
+              title: Text("hello world text"),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.check_box_outline_blank_rounded),
+              title: Text("hello world text"),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.check_box_outline_blank_rounded),
+              title: Text("hello world text"),
+            ),
+          ),
+        ],
+      ),
       appBar: AppBar(title: const Text('Transferências')),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
